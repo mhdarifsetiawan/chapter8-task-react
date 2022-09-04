c1a55f6310dadcced2cc62a546a19d81c9f5cb80
Muhammad Arif Setiawan <ariefroyal@gmail.com>
2022-09-05 06:21:44 +0700

    merubah halaman homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="mb3">
+        <h1 className="h1Title ptb3">Home Page</h1>
+      </div>
+    </>
+  );
+};
+export default Home;
